641e6c671b50bbd3b88bf3d468cdcf1a8b891463
Arran Schlosberg <me@arranschlosberg.com>
2010-12-13 10:56:50 +1000

    License - GPL v3

--- models/pass.js
+++ models/pass.js
@@ -1,3 +1,24 @@
+/*
+
+Copyright 2010 Arran Schlosberg (http://arranschlosberg.com);
+
+This file is part of NodeJS-Keychain (https://github.com/aschlosberg/NodeJS-Keychain).
+
+    NodeJS-Keychain is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    NodeJS-Keychain is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NodeJS-Keychain.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 var	fn = require('../functions.js'),
 		hash = fn.hash
 		crypto = require('crypto');
